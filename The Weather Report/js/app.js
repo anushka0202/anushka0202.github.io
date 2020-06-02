fd0e8e721eab8cb3bf09aedea049f36cdb874e16
anushka0202 <65349856+anushka0202@users.noreply.github.com>
2020-06-02 23:57:25 +0530

    Add files via upload

---
+++ The Weather Report/js/app.js
@@ -0,0 +1,42 @@
+//var search = document.getElementById('searchButton');
+//search.onclick = function () {
+//  weatherSearch();
+//};
+
+function weatherSearch() {
+  var city = document.getElementById('searchText').value;
+
+  var image = document.createElement('img');
+  image.setAttribute('id', 'icon');
+  document.getElementById('weather-container').appendChild(image);
+
+  var getWeather = document.createElement('p');
+  getWeather.setAttribute('id', 'weather');
+  document.getElementById('weather-container').appendChild(getWeather);
+
+  var getTemp = document.createElement('p');
+  getTemp.setAttribute('id', 'temp');
+  document.getElementById('weather-container').appendChild(getTemp);
+
+  axios
+    .get(
+      'http://api.openweathermap.org/data/2.5/weather?q=' +
+        city +
+        '&units=Imperial&APPID=33097b60432e0aeb705db4086da24fbb'
+    )
+    .then((res) => {
+      console.log(res);
+
+      var icon =
+        'http://api.openweathermap.org/img/w/' +
+        res.data.weather[0].icon +
+        '.png';
+      var temp = res.data.main.temp;
+      var weather = res.data.weather[0].main;
+
+      $('#icon').attr('src', icon);
+      $('#weather').append(weather);
+      $('#temp').append(temp);
+    })
+    .catch((err) => console.log(err));
+}
